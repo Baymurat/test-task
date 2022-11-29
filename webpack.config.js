cc0747daa8fb2272c234de245e12c9f2cb96ee34
Baymurat <baymurat.orazov@mail.ru>
2022-11-29 10:53:28 +0500

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env) => {
+  return {
+    entry: './src/index.tsx',
+    devtool: 'source-map',
+    mode: 'development',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      })
+    ],
+    resolve: {
+      extensions: ['.ts', '.js', '.tsx', '.jsx']
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    devServer: {
+      static: './dist'
+    }
+  }
+}
